efc1c0a447ac8760496e465726ffed3a1f4f62e4
Juan Carlos <juank897@hotmail.com>
2022-09-12 13:59:08 -0500

    Formulario veterinario

--- CuidadoPorcino.App/CuidadoPorcino.App.Frontend/Pages/Opciones/Veterinario.cs
+++ CuidadoPorcino.App/CuidadoPorcino.App.Frontend/Pages/Opciones/Veterinario.cs
@@ -5,12 +5,9 @@ namespace CuidadoPorcino.App.Frontend.Pages
 {
     public class VeterinarioModel : PageModel
     {
-        private string[] vectorOpciones = { "Opcion 1", "Opcion 2", "Opcion 3" };
-        public List<string> myLista { get; set; }
         public void OnGet()
         {
-            myLista = new List<string>();
-            myLista.AddRange(vectorOpciones);
+           
         }
     }
 }
