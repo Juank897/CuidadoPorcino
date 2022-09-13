35de531bf5d018163c8a8170c6c8ba482ba9a79a
nelsonsalazarlugo <nelsonsl55@gmail.com>
2022-09-12 20:48:14 -0500

    Version1

---
+++ Pages/Opciones/Cerdos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CuidadoPorcino.App.Fronten.Pages
+{
+    public class CerdosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
