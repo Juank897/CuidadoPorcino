e5f432027aba7e9b9fdb73828ba6e36ff55599df
Juan Carlos <juank897@hotmail.com>
2022-11-09 16:00:14 -0500

    Adicion linea

--- CuidadoPorcino.App/CuidadoPorcino.App.Dominio/Entidades/Cerdo.cs
+++ CuidadoPorcino.App/CuidadoPorcino.App.Dominio/Entidades/Cerdo.cs
@@ -20,6 +20,8 @@ namespace CuidadoPorcino.App.Dominio
         //public Veterinario veterinario {set;get;}
         //public Veterinario veterinario {set;get;}
 
+        //public Veterinario veterinario {set;get;}
+
 
         //public Propietario propietario {set;get;}
         //public Veterinario veterinario {set;get;}
